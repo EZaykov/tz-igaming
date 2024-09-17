00b471672669d49bc459480c597e6e432f977ea8
Evgeniy Zaykov <altqq4975@proton.me>
2024-09-17 11:52:36 +0700

    added upsert

--- src/domain/UserNumber.Repo.ts
+++ src/domain/UserNumber.Repo.ts
@@ -1,6 +1,7 @@
 import type { UserNumber } from "./UserNumber";
 
 export interface UserNumberRepo {
+	upsert(userNumber: UserNumber): Promise<void>;
 	findByTgId(tgId: number): Promise<UserNumber | null>;
 	deleteAllExpired(): Promise<void>;
 }
